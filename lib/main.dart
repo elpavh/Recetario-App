8d0d139b2a67b672dd1367309ddbc36512421a3c
elpavh <elpavh@gmail.com>
2025-08-27 07:57:59 -0600

    Creando homescreen para guardar pantallas

--- lib/main.dart
+++ lib/main.dart
@@ -7,13 +7,16 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false,title: 'Hola Mundo', 
-    home:RecipeBook() );
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Hola Mundo',
+      home: RecipeBook(),
+    );
   }
 }
 
 //Vamos a utilizar una extension de lo que seria un statelessWidget que por ahora lo tomaremos como una clase dinamica, lo llamaeros
-//my app 
+//my app
 
 class RecipeBook extends StatelessWidget {
   const RecipeBook({super.key});
@@ -23,31 +26,35 @@ class RecipeBook extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.blueAccent,
-        title: Text(
-          'Recipe Book',
-          style: TextStyle(color: Colors.white,
-          ),
-          ),
-          ),
-          body: Container(
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                image: NetworkImage('https://images.unsplash.com/photo-1504674900247-0877df9cc836?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80'),
-                fit: BoxFit.cover,
-              ),
-            ),
-            child: Center(
-              child: Text(
-                'Welcome to the Recipe Book',
-                style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
-                  backgroundColor: Colors.black45,
+        title: Text('Recipe Book', style: TextStyle(color: Colors.white)),
+      ),
+      body: Container(width:MediaQuery.of(context).size.width,
+      height: 125,
+        child: Card(
+          child: Row(
+            children: <Widget>[
+              Container(
+                height: 125,
+                width: 100,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(10.0),
+                  child: Container(),
                 ),
               ),
-            ),
+              SizedBox(width: 26),
+              Column(children: <Widget> [
+                Text("Lasgna"),
+                Text('Pablo V.'),
+                Container(
+                  height: 2,
+                  width: 75,
+                  color: Colors.orange
+                )
+              ],)
+            ],
           ),
-          );
+        ),
+      ),
+    );
   }
 }
