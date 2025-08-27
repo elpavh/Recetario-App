17e00eb2875e8e5a13a9c36ceba5f3f686d0d702
elpavh <elpavh@gmail.com>
2025-08-27 08:39:03 -0600

    movimos el container de la pantalla principal a la carpeta screen, en este caso lo movimos a la clase homescreen que extiende una clase de StatelessWidget, la cual se manda a llamar a travez de la TapBar

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:recipe_book/screens/home_screen.dart';
 
 void main() => runApp(const MyApp());
 
@@ -23,37 +24,26 @@ class RecipeBook extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.blueAccent,
-        title: Text('Recipe Book', style: TextStyle(color: Colors.white)),
-      ),
-      body: Container(width:MediaQuery.of(context).size.width,
-      height: 125,
-        child: Card(
-          child: Row(
-            children: <Widget>[
-              Container(
-                height: 125,
-                width: 100,
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(10.0),
-                  child: Container(),
-                ),
-              ),
-              SizedBox(width: 26),
-              Column(children: <Widget> [
-                Text("Lasgna"),
-                Text('Pablo V.'),
-                Container(
-                  height: 2,
-                  width: 75,
-                  color: Colors.orange
-                )
-              ],)
-            ],
-          ),
+    return DefaultTabController(
+      length: 1,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.blueAccent,
+          title: Text(
+            'Recipe Book', 
+            style: TextStyle(color:
+             Colors.white)
+             ),
+          bottom: TabBar(
+            indicatorColor: Colors.white,
+            labelColor: Colors.white,
+            unselectedLabelColor: Colors.white70,
+            tabs: [
+            Tab(icon: Icon(Icons.home), text: 'Home',),
+          ]),
         ),
+        body: TabBarView(children: [HomeScreen()],),
+        
       ),
     );
   }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -5,6 +5,35 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body:Container(
+        width:MediaQuery.of(context).size.width,
+        height: 125,
+        child: Card(
+              child: Row(
+                children: <Widget>[
+                  Container(
+                    height: 125,
+                    width: 100,
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(10.0),
+                      child: Container(),
+                    ),
+                  ),
+                  SizedBox(width: 26),
+                  Column(children: <Widget> [
+                    Text("Lasgna"),
+                    Text('Pablo V.'),
+                    Container(
+                      height: 2,
+                      width: 75,
+                      color: Colors.orange
+                    )
+                  ],)
+                ],
+              ),
+            ),
+          )
+          );
   }
 }
