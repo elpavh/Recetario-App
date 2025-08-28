b2a41da8693cc02abff2d5f4920034b65381b648
elpavh <elpavh@gmail.com>
2025-08-28 06:07:19 -0600

    Se agrega la imagen en la ruta del archivo pubsepc y regresamos el ClipRReact para poner la iimagen dentro

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -26,27 +26,27 @@ class HomeScreen extends StatelessWidget {
                     Container(
                       height: 125,
                       width: 100,
-                      decoration: BoxDecoration(
-                        color: Colors.orange,
-                        borderRadius: BorderRadius.only( // se agrego borderRadius.only para que las esquinas izquierdas del contenedor esten redondeadas
+                      child: ClipRRect( // se agrego ClipRRect para que la imagen tenga las esquinas redondeadas
+                        borderRadius: BorderRadius.only( 
                           topLeft: Radius.circular(12),
                           bottomLeft: Radius.circular(12)
                         ),
+                        child: Image.asset('assets/images/lasagna.jpg', fit: BoxFit.cover),
                       ),
                     ),
                     SizedBox(width: 26),
                     Column(
-                      mainAxisAlignment: MainAxisAlignment.center, // se agrego para que se centre la informacion en el medio
-                      crossAxisAlignment: CrossAxisAlignment.start,// Siempre de manera horizontal este ubicado al inicio
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: <Widget> [
-                        Text("Lasgna", style: TextStyle(fontSize: 16,fontFamily: 'Quicksan',fontWeight: FontWeight.bold)), // se agrego fontWeight.bold para que el nombre de la receta se vea en negrita
-                        SizedBox(height: 4,), // Se agregaron sizebox en height esto porque estamos usando una columna para dar espacio
-                        Container( // Movimos de lugar el container para que la linea naranja divida el nobmre del autor
+                        Text("Lasgna", style: TextStyle(fontSize: 16,fontFamily: 'Quicksan',fontWeight: FontWeight.bold)),
+                        SizedBox(height: 4,), 
+                        Container( 
                           height: 2,
                           width: 75,
                           color: Colors.orange
                         ),
-                        Text('Pablo V.', style: TextStyle(fontSize: 16,fontFamily: 'Quicksan')), // se agrego TextStyle para darle estilo y fuente
+                        Text('Pablo V.', style: TextStyle(fontSize: 16,fontFamily: 'Quicksan')), 
                         SizedBox(height: 4,),
                         
                     ],)
